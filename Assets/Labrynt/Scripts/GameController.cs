fdf1165c7901129cdf4451d72d62e294e29ccd17
Michal Slawinski <michal.slawin@gmail.com>
2020-12-29 15:27:03 +0100

    created GameController.cs

---
+++ Assets/Labrynt/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private static int maxFps = 60;
+
+    public static int MaxFps { get => maxFps; set => maxFps = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = MaxFps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
