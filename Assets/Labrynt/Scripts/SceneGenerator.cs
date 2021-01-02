81220f919532bcfa419b86b5380a702c91b8ed23
Michal Slawinski <michal.slawin@gmail.com>
2021-01-02 12:59:51 +0100

    started SceneGenerator functionality

--- Assets/Labrynt/Scripts/SceneGenerator.cs
+++ Assets/Labrynt/Scripts/SceneGenerator.cs
@@ -8,7 +8,12 @@ public class SceneGenerator : MonoBehaviour
 
     public Corridor deadEndPrefab;
     public Corridor tripleCorridorPrefab;
-    public Corridor fallingSandTrapPrefab;
+    public Corridor finishEndPrefab;
+    public Corridor corridor4x4Prefab;
+
+    public Trap fallingSandTrapPrefab;
+    public Trap bladesTrapPrefab;
+    public Trap floorSpikesTrapPrefab;
 
     private Corridor lastPlaced;
 
@@ -17,15 +22,28 @@ public class SceneGenerator : MonoBehaviour
     {
         lastPlaced = start;
 
-        PlaceCorridor(fallingSandTrapPrefab);
-        PlaceCorridor(tripleCorridorPrefab);
-        PlaceCorridor(deadEndPrefab);
+        PlaceCorridorFrontally(bladesTrapPrefab);
+        PlaceCorridorFrontally(corridor4x4Prefab);
+        PlaceCorridorFrontally(floorSpikesTrapPrefab);
+        PlaceCorridorBackwards(finishEndPrefab);
     }
 
-    private void PlaceCorridor(Corridor corridorPrefab)
+    private void PlaceCorridor(Corridor corridorPrefab, Quaternion rotation)
     {
         lastPlaced = Instantiate(corridorPrefab,
             new Vector3(lastPlaced.transform.position.x + lastPlaced.joint1OffsetX, lastPlaced.transform.position.y, lastPlaced.transform.position.z + lastPlaced.joint1OffsetZ),
-            Quaternion.identity);
+            rotation);
+    }
+
+    private void PlaceCorridorFrontally(Corridor corridorPrefab)
+    {
+        PlaceCorridor(corridorPrefab, Quaternion.identity);
+    }
+
+    private void PlaceCorridorBackwards(Corridor corridorPrefab)
+    {
+        lastPlaced.joint1OffsetX += corridorPrefab.joint1OffsetX + 2;
+        lastPlaced.joint1OffsetZ -= 2;
+        PlaceCorridor(corridorPrefab, Quaternion.Euler(0f, 180f, 0f));
     }
 }
